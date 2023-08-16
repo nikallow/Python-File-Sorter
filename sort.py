7bb4c9d1acc74486d0806131a97742c934d9e533
nikallow <135876929+nikallow@users.noreply.github.com>
2023-08-16 21:38:48 +0300

    Added categories and threading

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from threading import Thread
 
 def sort_files(folder_location):
 
@@ -31,7 +32,33 @@ def sort_files(folder_location):
                 file_path = os.path.join(folder_location, file_name)
                 new_file_path = os.path.join(folder, new_file_name)
                 shutil.move(file_path, new_file_path)
+
+
+    images_types = (".png", ".jpg", ".bmp", ".gif", ".tif", ".kra", ".svg",
+                     ".raw", ".tiff", ".psd", ".bmp", ".jpeg", ".webp")
     
-    # Sorting Images
-    sorting("Images", (".png", ".jpg", ".bmp", ".gif", ".tif", ".kra", ".svg",
-                        ".raw.", ".tiff", ".psd", ".bmp", ".jpeg", ".webp"))
+    documents_types = (".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx",
+                        ".pdf", ".djvu", ".fb2", ".epub", ".mobi", ".txt",
+                        ".odt", ".ods", ".odp", ".odg", ".odf", ".odb")
+    
+    archives_types = (".zip", ".7z", ".rar", ".tar", ".gz", ".tar.gz")
+
+    videos_types = (".mp4", ".mkv", ".mov", ".hevc")
+
+    music_types = (".mp3", ".ogg", ".wav", ".aiff", ".ape", ".flac")
+
+
+    Images = Thread(target=sorting("Images", images_types))
+    Images.start()
+    
+    Documents = Thread(target=sorting("Documents", documents_types))
+    Documents.start()
+
+    Archives = Thread(target=sorting("Archives", archives_types))
+    Archives.start()
+
+    Videos = Thread(target=sorting("Videos", videos_types))
+    Videos.start()
+
+    Music = Thread(target=sorting("Music", music_types))
+    Music.start()
