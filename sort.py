12b94595fb9667695589f106c3e404bc2c77e701
nikallow <135876929+nikallow@users.noreply.github.com>
2023-08-16 19:23:13 +0300

    Added checking for files

--- sort.py
+++ sort.py
@@ -3,19 +3,25 @@ import shutil
 
 def sort_files(folder_location):
 
-    def sorting(folder_name, file_types):    
-        folder = os.path.join(sorted_folder, folder_name)
-        os.makedirs(folder, exist_ok=True)
-
+    def sorting(folder_name, file_types):
         for file_name in os.listdir(folder_location):
-            if file_name.endswith(file_types):
+
+            # Check if there are files with the received extensions
+            if file_name.endswith(file_types): 
+
+                # Creating a Sortred folder if it doesn't exist
+                sorted_folder = os.path.join(folder_location, "Sorted")
+                os.makedirs(sorted_folder, exist_ok=True)   
+
+                # Creating a folder for a category if it does not exist
+                folder = os.path.join(sorted_folder, folder_name)
+                os.makedirs(folder, exist_ok=True)
+
+                # Moving files to a folder
                 file_path = os.path.join(folder_location, file_name)
                 shutil.move(file_path, folder)
 
-    # Creating a Sortred folder
-    sorted_folder = os.path.join(folder_location, "Sorted")
-    os.makedirs(sorted_folder, exist_ok=True)   
-
+    
     # Sorting Images
     sorting("Images", (".png", ".jpg", ".bmp", ".gif", ".tif", ".kra", ".svg",
                         ".raw.", ".tiff", ".psd", ".bmp", ".jpeg", ".webp"))
