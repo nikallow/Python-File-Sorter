41187ff5fe6b75f1e17f3a99ee9c451e5a66af1c
nikallow <135876929+nikallow@users.noreply.github.com>
2023-08-16 18:17:15 +0300

    The code now conforms to the DRY principle

--- sort.py
+++ sort.py
@@ -2,15 +2,20 @@ import os
 import shutil
 
 def sort_files(folder_location):
+
+    def sorting(folder_name, file_types):    
+        folder = os.path.join(sorted_folder, folder_name)
+        os.makedirs(folder, exist_ok=True)
+
+        for file_name in os.listdir(folder_location):
+            if file_name.endswith(file_types):
+                file_path = os.path.join(folder_location, file_name)
+                shutil.move(file_path, folder)
+
+    # Creating a Sortred folder
     sorted_folder = os.path.join(folder_location, "Sorted")
-    os.makedirs(sorted_folder, exist_ok=True)
+    os.makedirs(sorted_folder, exist_ok=True)   
 
-    images_folder = os.path.join(sorted_folder, "Images")
-    os.makedirs(images_folder, exist_ok=True)
-
-    for file_name in os.listdir(folder_location):
-        if file_name.endswith((".png", ".jpg", ".bmp", ".gif", ".tif",
-                              ".kra", ".svg", ".raw.", ".tiff", ".psd",
-                              ".bmp", ".jpeg", ".webp")):
-            file_path = os.path.join(folder_location, file_name)
-            shutil.move(file_path, images_folder)
+    # Sorting Images
+    sorting("Images", (".png", ".jpg", ".bmp", ".gif", ".tif", ".kra", ".svg",
+                        ".raw.", ".tiff", ".psd", ".bmp", ".jpeg", ".webp"))
