5ac84eaba246ed9a52bd3c24cc667523d7be431a
nikallow <nikallowdev@gmail.com>
2023-08-17 14:59:33 +0300

    Changed the color of the buttons to dark blue

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 from app import App
 
 ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("blue")
+ctk.set_default_color_theme("dark-blue")
 
 
 if __name__ == "__main__":
